7d10d35ba45483bf3cc9de91ae8d8875726ade3c
joydeep guha <joydeep1st@gmail.com>
2018-07-15 01:31:40 +0530

    second commit

--- AtmAndBranchLocator/src/Utility/ConfigProvider.java
+++ AtmAndBranchLocator/src/Utility/ConfigProvider.java
@@ -3,10 +3,10 @@ package Utility;
 public interface ConfigProvider {
 	
 	String Driver = "com.mysql.jdbc.Driver";
-	String dbuname = "root";
-	String dbpassword = "";
-	String database ="db_atm_branch_locator";
-	String db_atm_branch_locator_Con_URL ="jdbc:mysql://localhost:3306/db_atm_branch_locator";
+	String dbuname = "jibendu123";
+	String dbpassword = "jibendu123";
+	String database ="atmbankloc";
+	String db_atm_branch_locator_Con_URL ="jdbc:mysql://atmbankloc.cekttczubrid.us-east-2.rds.amazonaws.com:3306/atmbankloc";
 	String api_Barclays_atm = "https://atlas.api.barclays/open-banking/v2.1/atms";
 
 }
