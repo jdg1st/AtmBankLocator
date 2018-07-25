7aebc6e436041c85d49fdfcb14d80dfaff5a3413
jdg1st <40295818+jdg1st@users.noreply.github.com>
2018-07-25 14:05:11 +0530

    Update ConfigDriver.java

--- AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
+++ AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
@@ -18,7 +18,7 @@ public class ConfigDriver  {
 	public WebDriver getSetDriver() throws MalformedURLException {
 		String workingDir = System.getProperty("user.dir");
 //		System.setProperty("webdriver.chrome.driver",workingDir+File.separator+"src"+File.separator+"test"+File.separator+"resources"+File.separator+"BrowserDrivers"+File.separator+"chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","//home//ubuntu//chromedriver");
+		System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");
 		ChromeOptions opt= new ChromeOptions();
 		
 		
