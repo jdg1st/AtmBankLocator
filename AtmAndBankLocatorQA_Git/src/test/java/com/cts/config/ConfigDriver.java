8f79f7b9995206612beeebd3488f9db872ba6c0c
jdg1st <40295818+jdg1st@users.noreply.github.com>
2018-07-25 15:51:50 +0530

    Update ConfigDriver.java

--- AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
+++ AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
@@ -1,12 +1,12 @@
 package com.cts.config;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -18,25 +18,27 @@ public class ConfigDriver  {
 	public WebDriver getSetDriver() throws MalformedURLException {
 		String workingDir = System.getProperty("user.dir");
 //		System.setProperty("webdriver.chrome.driver",workingDir+File.separator+"src"+File.separator+"test"+File.separator+"resources"+File.separator+"BrowserDrivers"+File.separator+"chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");
-		ChromeOptions opt= new ChromeOptions();
-		
-		
+		//System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");		
+		//ChromeOptions opt= new ChromeOptions();	
 		//DesiredCapabilities desiredCap = DesiredCapabilities.chrome();
 		//desiredCap.setCapability("--headless", true);
-
         //final URL url = new URL("http://18.188.250.212:8080/AtmAndBranchLocator.war/");
         //driver = new RemoteWebDriver(url, desiredCap);
-
 		//opt.addArguments("port=4444");
-		opt.addArguments("--headless");
+		//opt.addArguments("--headless");
 //		DesiredCapabilities desired = new DesiredCapabilities();		
 //		desired.setCapability(ChromeOptions.CAPABILITY, opt);
-
-		driver = new ChromeDriver(opt);
+		//driver = new ChromeDriver(opt);
 		//driver = new RemoteWebDriver(desired);
 		//driver = new ChromeDriver();
 
+		System.setProperty("webdriver.gecko.driver", "/home/ubuntu/geckodriver.exe");
+		FirefoxOptions fo = new FirefoxOptions();
+		fo.addArguments("--headless");
+		
+		driver = new FirefoxDriver(fo);
+		
+		
 		return driver;
 	}
 
