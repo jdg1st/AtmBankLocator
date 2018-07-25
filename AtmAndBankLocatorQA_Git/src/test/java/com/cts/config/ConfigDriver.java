a8cde2eb6a5466f7ff443fca1954dfe53626f241
jdg1st <40295818+jdg1st@users.noreply.github.com>
2018-07-25 18:27:09 +0530

    Update ConfigDriver.java

--- AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
+++ AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
@@ -40,11 +40,13 @@ public class ConfigDriver  {
 // 		driver = new FirefoxDriver(fo);
 
 // 		System.setProperty("phantomjs.binary.path", "/usr/local/bin/phantomjs");
-		System.setProperty("phantomjs.binary.path", "/usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
+// 		System.setProperty("phantomjs.binary.path", "/usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
+		File path=new File("/usr/local/bin/phantomjs");
+		System.setProperty("phantomjs.binary.path", path.getAbsolutePath());
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		desiredCapabilities.setJavascriptEnabled(true);
 		String [] phantomJsArgs = {"--ignore-ssl-errors=yes"};
-		desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_CLI_ARGS, phantomJsArgs);
+		desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomJsArgs);
 		driver = new PhantomJSDriver(desiredCapabilities);
 		
 		
