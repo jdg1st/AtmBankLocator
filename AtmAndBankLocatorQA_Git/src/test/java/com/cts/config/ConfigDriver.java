ac4ddd912dcc861d074b7844270e28b7cd7fa433
jdg1st <40295818+jdg1st@users.noreply.github.com>
2018-07-25 19:27:13 +0530

    Update ConfigDriver.java

--- AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
+++ AtmAndBankLocatorQA_Git/src/test/java/com/cts/config/ConfigDriver.java
@@ -3,15 +3,15 @@ package com.cts.config;
 import java.io.File;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxOptions;
+// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+// import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+// import org.openqa.selenium.remote.DesiredCapabilities;
+// import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class ConfigDriver  {
 	
